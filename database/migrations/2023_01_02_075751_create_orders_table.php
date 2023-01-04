14f4d51518dd78d18ba9ee29f321bacb3ba79d46
Yudhistira Haryatmaka Santoso <91047747+disyudhiss@users.noreply.github.com>
2023-01-04 13:39:43 +0700

    migration orders

--- database/migrations/2023_01_02_075751_create_orders_table.php
+++ database/migrations/2023_01_02_075751_create_orders_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
+            $table->integer('jumlahAtasan');
+            $table->integer('jumlahBawahan');
             $table->tinyInteger('warnaPakaian');
             $table->tinyInteger('paketPewangi');
             $table->timestamps();
