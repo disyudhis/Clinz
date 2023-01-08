171e6707a7f8644d48265bda5d72113950e5bfb2
Yudhistira Haryatmaka Santoso <91047747+disyudhiss@users.noreply.github.com>
2023-01-08 23:02:30 +0700

    userType for users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->unique();
+            $table->string('userType')->default(0);
             $table->string('noTelp');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
